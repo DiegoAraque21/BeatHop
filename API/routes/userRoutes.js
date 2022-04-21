3cdd839432021470ed13211e32bcbf04e6a23da1
Marco Torres <marcotorres258@outlook.com>
2022-04-21 12:33:52 -0500

    Form and user post endpoints

--- API/routes/userRoutes.js
+++ API/routes/userRoutes.js
@@ -3,35 +3,46 @@ let router = express.Router();
 let path = require("path");
 let connectToDB = require("../db");
 
+
 // Log In
 router.get("/login", function (req, res) {
   res.sendFile(path.join(__dirname, "../views/login.html"));
 });
+
 router.post("/login", function (req, res) {});
+
 
 // Create Account
 router.get("/create_account", function (req, res) {
   res.sendFile(path.join(__dirname, "../views/createAccount.html"));
 });
-router.post("/create_account", function (req, res) {});
 
-// New Form
-router.post("/form", function (req, res) {  
+router.post("/create_account", function (req, res) {
   try {
     // Validate fields
-    if (!req.body.answer1 || !req.body.answer2 || !req.body.answer3) throw "You must complete all fields.";
+    if (!req.body.name || !req.body.lastName || !req.body.email || !req.body.password ||
+       !req.body.type || !req.body.gender || !req.body.age || !req.body.idForm)
+      throw "You must complete all fields.";
 
     // Make connection
     let connection = connectToDB();
     connection.connect();
 
-    // Create query
-    let query = `INSERT INTO Form (answer1, answer2, answer3) VALUES ("${req.body.answer1}", "${req.body.answer2}", "${req.body.answer3}")`;
+    // Create user query
+    let userQuery = `INSERT INTO User (name, lastName, email, password, userType, age, gender, idForm) VALUES 
+    ("${req.body.name}", "${req.body.lastName}", "${req.body.email}", "${req.body.password}", "${req.body.type}", ${req.body.age}, "${req.body.gender}", ${req.body.idForm})`;
 
-    // Execute query in DB
-    connection.query(query, (error, results) => {
-      if (error) throw "There was an error with the creation of the user.";
-      res.status(200).json({ message: "Form created successfully.", data: results });
+    console.log(userQuery);
+
+    // Execute from query in DB
+    connection.query(userQuery, (error, results) => {
+
+      // If error creating user
+      if (error) throw "There was an error with the creation of the user."; 
+      
+      // Response
+      res.status(200).json({ message: "User created successfully.", userId: results.insertId });
+
     });
 
     // End connection
@@ -40,7 +51,43 @@ router.post("/form", function (req, res) {
 
   // Manage error
   catch (error) {
-    console.log("INSERT FORM ERROR:", error);
+    console.log("INSERT USER ERROR:", error);
+    res.status(500).json({ error });
+  }
+});
+
+// New form
+router.post("/form", function (req, res) {
+  try {
+    // Validate fields
+    if (!req.body.answer1 || !req.body.answer2 || !req.body.answer3) throw "You must complete all fields.";
+
+    // Make connection
+    let connection = connectToDB();
+    connection.connect();
+
+    // Create form query
+    let formQuery = `INSERT INTO Form (answer1, answer2, answer3) VALUES 
+    ("${req.body.answer1}", "${req.body.answer2}", "${req.body.answer3}")`;
+
+    // Execute from query in DB
+    connection.query(formQuery, (error, results) => {
+      
+      // If error creating form
+      if (error) throw "There was an error with the creation of the form.";
+
+      // Response
+      res.status(200).json({ message: "Form created successfully.", formId: results.insertId});
+
+    });
+    
+    // End connection
+    connection.end();
+  }
+
+  // Manage error
+  catch (error) {
+    console.log("INSERT USER ERROR:", error);
     res.status(500).json({ error });
   }
 });
