7a42123ed3f1b8555de37ce60ab21b7bc435f7f2
Marco Torres <marcotorres258@outlook.com>
2022-04-25 14:14:14 -0500

    Connect game graphs with back

--- API/public/js/graphsScripts/gameGraphs.js
+++ API/public/js/graphsScripts/gameGraphs.js
@@ -1,95 +1,157 @@
-// Graph 1
-let game1 = document.getElementById("game1").getContext("2d");
-let myChart = new Chart(game1, {
-    type: "bar",
-    data: {
-        labels: ['Level 1', 'Level 2', 'Level 3'],
-        datasets: [{
-            label: 'Average Score',
-            data: [10, 9, 15],
-            backgroundColor: [
-                'rgb(74, 135, 72,0.5)',
-                'rgb(74, 135, 72,0.5)',
-                'rgb(74, 135, 72,0.5)'
-            ]
-        }]
+// Dataset
+const gameDataset = [
+    {
+        title: "Average score per level",
+        label: "Average Score",
+        canvasId: "game1",
+        answers: [
+            {
+                num: 10,
+                label: "Level 1",
+                color: "rgba(54, 162, 235, 0.2)",
+                border: "rgb(54, 162, 235)"
+            },
+            {
+                num: 8,
+                label: "Level 2",
+                color: "rgba(54, 162, 235, 0.2)",
+                border: "rgb(54, 162, 235)"
+            },
+            {
+                num: 7,
+                label: "Level 3",
+                color: "rgba(54, 162, 235, 0.2)",
+                border: "rgb(54, 162, 235)"
+            }
+        ]
     },
-    options: {
-        title: {
-            display: true,
-            text: 'Average score per level'
-        },
-        maintainAspectRatio: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
+    {
+        title: "Total deaths per level",
+        label: "Total Deaths",
+        canvasId: "game2",
+        answers: [
+            {
+                num: 10,
+                label: "Level 1",
+                color: "rgba(255, 99, 132, 0.2)",
+                border: "rgb(255, 99, 132)"
+            },
+            {
+                num: 8,
+                label: "Level 2",
+                color: "rgba(255, 99, 132, 0.2)",
+                border: "rgb(255, 99, 132)"
+            },
+            {
+                num: 7,
+                label: "Level 3",
+                color: "rgba(255, 99, 132, 0.2)",
+                border: "rgb(255, 99, 132)"
+            }
+        ]
+    },
+    {
+        title: "Total tries per level",
+        label: "Total Tries",
+        canvasId: "game3",
+        answers: [
+            {
+                num: 10,
+                label: "Level 1",
+                color: "rgba(255, 205, 86, 0.2)",
+                border: "rgb(255, 205, 86)"
+            },
+            {
+                num: 8,
+                label: "Level 2",
+                color: "rgba(255, 205, 86, 0.2)",
+                border: "rgb(255, 205, 86)"
+            },
+            {
+                num: 7,
+                label: "Level 3",
+                color: "rgba(255, 205, 86, 0.2)",
+                border: "rgb(255, 205, 86)"
+            }
+        ]
     }
-});
+]
 
-// Graph 2
-let game2 = document.getElementById("game2").getContext("2d");
-let myChart2 = new Chart(game2, {
-    type: "bar",
-    data: {
-        labels: ['Level 1', 'Level 2', 'Level 3'],
-        datasets: [{
-            label: 'Total Deaths',
-            data: [10, 9, 15],
-            backgroundColor: [
-                'rgb(229, 89, 50,0.5)',
-                'rgb(229, 89, 50,0.5)',
-                'rgb(229, 89, 50,0.5)'
-            ]
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Total deaths per level'
-        },
-        maintainAspectRatio: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// Build game graphs
+async function buildGameGraphs() {
+    try {
+        // Send request
+        let resGameDB = await fetch("http://localhost:3000/game/all_level");
+        let resGame = await resGameDB.json();
 
-// Graph 3
-let game3 = document.getElementById("game3").getContext("2d");
-let myChart3 = new Chart(game3, {
-    type: "bar",
-    data: {
-        labels: ['Level 1', 'Level 2', 'Level 3'],
-        datasets: [{
-            label: 'Total Tries',
-            data: [10, 9, 15],
-            backgroundColor: [
-                'rgb(66, 134, 244,0.5)',
-                'rgb(66, 134, 244,0.5)',
-                'rgb(66, 134, 244,0.5)'
-            ]
-        }]
-    },
-    options: {
-        title: {
-            display: true,
-            text: 'Total tries per level'
-        },
-        maintainAspectRatio: true,
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+        // If request fails
+        if (resGame.error) throw resGame.error;
+
+        // Modify datasets
+        for (let i = 0; i < 3; i++){
+            gameDataset[0].answers[i].num = resGame.levels[i].avgScore;
+            gameDataset[1].answers[i].num = resGame.levels[i].totalDeaths;
+            gameDataset[2].answers[i].num = resGame.levels[i].totalTries;
         }
+
+        // Build graphs with the dataset
+        gameDataset.forEach(graph => {
+            
+            // Get the canvas
+            let canvas = document.getElementById(graph.canvasId).getContext("2d");
+
+            // Create chart
+            let chart = new Chart(canvas, {
+                type: "bar",
+                data: {
+                    labels: [
+                        graph.answers[0].label,
+                        graph.answers[1].label,
+                        graph.answers[2].label
+                    ],
+                    datasets: [{
+                        label: graph.label,
+                        data: [
+                            graph.answers[0].num,
+                            graph.answers[1].num,
+                            graph.answers[2].num
+                        ],
+                        backgroundColor: [
+                            graph.answers[0].color,
+                            graph.answers[1].color,
+                            graph.answers[2].color
+                        ],
+                        borderColor: [
+                            graph.answers[0].border,
+                            graph.answers[1].border,
+                            graph.answers[2].border
+                        ],
+                        borderWidth: 1.5
+                    }]
+                },
+                options: {
+                    title: {
+                        display: true,
+                        text: graph.title
+                    },
+                    maintainAspectRatio: true,
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+                }
+            });
+        })
+
     }
-});
+    catch (error) {
+        console.log("GET LEVEL DATA ERROR:", error);
+        alert(error);
+    }
+};
+
+// Build game graphs
+buildGameGraphs();
